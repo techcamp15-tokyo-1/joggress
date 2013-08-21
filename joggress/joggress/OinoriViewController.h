8592895f6dcef09aea4e1471e2e7a09633ef3fbe
0673nC <0673nC@0673nC-no-MacBook-Air.local>
2013-08-21 19:27:27 +0900

    project+

---
+++ joggress/joggress/OinoriViewController.h
@@ -0,0 +1,13 @@
+//
+//  OinoriViewController.h
+//  joggress
+//
+//  Created by 0673nC on 2013/08/21.
+//  Copyright (c) 2013年 0673nC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OinoriViewController : UIViewController
+- (IBAction)respondToButtonClick:(id)sender;
+@end
---
+++ joggress/joggress/OinoriViewController.m
@@ -0,0 +1,45 @@
+//
+//  OinoriViewController.m
+//  joggress
+//
+//  Created by 0673nC on 2013/08/21.
+//  Copyright (c) 2013年 0673nC. All rights reserved.
+//
+
+#import "OinoriViewController.h"
+
+@interface OinoriViewController ()
+
+@end
+
+@implementation OinoriViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)respondToButtonClick:(id)sender
+{
+    // 画面を閉じる処理
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
